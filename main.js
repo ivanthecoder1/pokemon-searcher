bab41ac95c41fbcdf04c36c11af379ebdeacbf43
Ivan Zhang <56855196+ivanthecoder1@users.noreply.github.com>
2022-08-14 21:27:08 -0400

    added back sprite

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// Add your code here!
-
-
 let submit = document.getElementById('submit');
-
 let clear = document.getElementById('clear');
 
 // submit pokemon name, and get data back
@@ -16,7 +12,7 @@ function onClick(event) {
             return response.json();
 
         })
-        .then(function (myJson) { // out put JSON data onto site
+        .then(function (pokemon) { // out put JSON data onto site
 
             // select Main Container from HTML
             let mainContainer = document.querySelector('.container');
@@ -26,50 +22,64 @@ function onClick(event) {
             subContainer.className = 'subContainer';
 
         // image section 
-            let imageContainer = document.createElement('div');
-            imageContainer.className = 'pokemonImage';
+            let imageContainer1 = document.createElement('div');
+            imageContainer1.className = 'pokemonImage';
+
+            let imageContainer2 = document.createElement('div');
+            imageContainer2.className = 'pokemonImage2';
 
             // extract image from API and append
-            let newImg = document.createElement('img');
-            newImg.src = myJson.sprites.front_default;
-            imageContainer.append(newImg);
+            let front_sprite = document.createElement('img');
+            front_sprite.src = pokemon.sprites.front_default;
 
+            let back_sprite = document.createElement('img');
+            back_sprite.src = pokemon.sprites.back_default;
+
+            imageContainer1.append(front_sprite)
+            imageContainer2.append(back_sprite);
+
+        
         // Info section
             let infoContainer = document.createElement('div');
             infoContainer.className = 'pokemonInfo';
 
             // Extract pokemon's name
             let pokeName = document.createElement('li');
-            pokeName.innerText = "Pokemon's Name: " + myJson.species.name;
+            pokeName.innerText = "Pokemon's Name: " + pokemon.species.name;
             infoContainer.append(pokeName);
            
             // Extract type information
             let typeInfo = document.createElement('li');
-            typeInfo.innerText = "Type: " + myJson.types[0].type.name;
+            typeInfo.innerText = "Type: " + pokemon.types[0].type.name;
             infoContainer.append(typeInfo);
                 //Future fix: Account if a pokemon has two types
             
 
             // Extract ability information
             let abilityInfo = document.createElement('li');
-            abilityInfo.innerText = "Abilities: " + myJson.abilities[0].ability.name;
+            abilityInfo.innerText = "Abilities: " + pokemon.abilities[0].ability.name;
             infoContainer.append(abilityInfo);
 
 
             // Loop through all six stats, and extract stat information, append info
             for (let i = 0; i < 6; i++) {
                     let stats = document.createElement('li');
-                    stat_name = myJson.stats[i].stat.name
-                    stats.innerText = stat_name +  " stat: " + myJson.stats[i].base_stat
+                    stat_name = pokemon.stats[i].stat.name
+                    stats.innerText = stat_name +  " stat: " + pokemon.stats[i].base_stat
                     infoContainer.append(stats)
             }
 
             // Add all info to containers and back to main containers
-            subContainer.append(imageContainer);
+            subContainer.append(imageContainer1);
+            subContainer.append(imageContainer2);
             subContainer.append(infoContainer);
             mainContainer.appendChild(subContainer);
          
         })
+        // error 
+        .catch(error => {
+            console.log('API failure');
+        });
 }
 
 // clear button
@@ -78,9 +88,9 @@ function onClick2 () {
     newP.innerText = '';
 }
 
-// Future tip: return error message if user incorrectly inputs pokemon name
-
+// connects to the html form
 submit.addEventListener('click', onClick);
 clear.addEventListener('click', onClick2);
+
 
 
